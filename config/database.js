c0430156ba466f3b3078506d9a4cb8a7ffb3f03e
harshikaS16 <39238203+harshikaS16@users.noreply.github.com>
2021-10-07 13:42:45 -0500

    Update database.js
    Hiding the mongodb URL

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-     database: "mongodb://faasos:faasos@ds219130.mlab.com:19130/faasosapp"
+     database: "mongodb://****************************"
     // database: "mongodb://localhost:27017/testDB"
 }   
